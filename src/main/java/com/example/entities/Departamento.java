7f9bbfc5bb051ece85eadc356adb10f87f9ec59a
SpSf2 <kiworks.duglas@gmail.com>
2026-06-12 13:35:34 +0200

    Entidades relacionadas y modificacion del application con el nombre de la DB y paswword

--- src/main/java/com/example/entities/Departamento.java
+++ src/main/java/com/example/entities/Departamento.java
@@ -1,9 +1,14 @@
 package com.example.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,4 +27,16 @@ public class Departamento {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String nombre;
+
+    /* Las relaciones entre las entidades jpa (JAVA PERSISNTECE api) son bidireccionales
+    a diferencia de las relaciones en SQL que son unidireccionales, es decir, que una 
+    entidad hija sabe quien es su padre, porque en dicha entidad es donde se crea la 
+    relación de clave externa (FK), pero la entidad Padre no sabe que tine hijo(s)  */
+
+    /* El atributo mappedBy apunta a una propiedad en el lado de muchos de la relacion, pues
+aunque las relaciones son bidireccionales hay que especificar donde se va a crear 
+la relacion de clave externa, que al igual que en SQL es en el lado de muchos */
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "departamento")
+    private List<Empleado> empleados;
 }
