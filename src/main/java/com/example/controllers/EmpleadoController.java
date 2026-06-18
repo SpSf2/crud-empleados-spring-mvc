7ee22bacb9546bb7f679b3a7ded04e0582d7090f
SpSf2 <kiworks.duglas@gmail.com>
2026-06-18 03:26:41 +0200

    ultimo antes de dar alta

--- src/main/java/com/example/controllers/EmpleadoController.java
+++ src/main/java/com/example/controllers/EmpleadoController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entities.Departamento;
+import com.example.entities.Empleado;
+import com.example.services.DepartamentoService;
 import com.example.services.EmpleadoService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EmpleadoController {
     
     private final EmpleadoService empleadoService;
+    private final DepartamentoService departamentoService;
+
 
     @GetMapping("/listar")
     public String listarEmpleados(Model model) {
@@ -26,4 +31,17 @@ public class EmpleadoController {
         return "listadoEmpleados";
     }
 
+    //metodo para mostrar el formulario de creación de empleados
+    @GetMapping("/alta")
+    public String mostrarFormularioAlta(Model model) {
+
+        //Se necesitan los Departamentos desde la capa de Servicios y lo inyectamos arriba
+        model.addAttribute("departamentos", departamentoService.getAllDepartamentos());
+
+        /*Se necesita enviar un Objeto Empleado vacio, para que se vinculen sus propiedades con cada control
+        (elemento, input, select, etc) del formulario */
+        model.addAttribute("empleado", new Empleado());
+
+        return "formularioAltaModificacion";
+    }
 }
